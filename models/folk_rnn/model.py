d84376f8678fa3f9bce2565f3054710ff3f200a1
Maksymilian Łazarski <01158804@pw.edu.pl>
2024-04-16 19:59:34 +0200

    test

--- models/folk_rnn/model.py
+++ models/folk_rnn/model.py
@@ -78,6 +78,7 @@ class FolkRNN(pl.LightningModule):
         self.log("val/loss", loss, prog_bar=True, logger=True, on_step=False, on_epoch=True)
         return loss
 
+    # fmt: off
     def configure_optimizers(self):
         optimizer = RMSprop(params=self.parameters(), lr=self.lr)
         scheduler = LambdaLR(
@@ -89,3 +90,4 @@ class FolkRNN(pl.LightningModule):
             ),
         )
         return {'optimizer': optimizer, 'lr_scheduler': scheduler}
+    # fmt: on
