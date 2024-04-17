b6f2caa53d65b2f1eb28bb55cebdba2eee627235
Maksymilian Łazarski <01158804@pw.edu.pl>
2024-04-17 21:39:36 +0200

    Format

--- scripts/converter.py
+++ scripts/converter.py
@@ -1,6 +1,6 @@
 from typing import List
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder  # type: ignore[import]
 from music21 import converter
 
 
@@ -17,22 +17,22 @@ class OneHotToMidiConverter:
         return [item for sublist in track_notes for item in sublist]
 
     def _reformat_notes(self, notes: List[str]):
-        notes.insert(1, 'L: 1/8')
-        notes.insert(3, '\n')
-        notes.insert(2, '\n')
-        notes.insert(1, '\n')
+        notes.insert(1, "L: 1/8")
+        notes.insert(3, "\n")
+        notes.insert(2, "\n")
+        notes.insert(1, "\n")
         return notes
 
-    def _convert_abc_to_midi(self, notes_string: str, filename: str = 'output.mid'):
+    def _convert_abc_to_midi(self, notes_string: str, filename: str = "output.mid"):
         stream = converter.parse(notes_string)
-        stream.write('midi', fp=filename)
+        stream.write("midi", fp=filename)
 
-    def __call__(self, encodings, filename='output.mid'):
+    def __call__(self, encodings, filename="output.mid"):
         notes = self._convert_ohc_to_str_list(encodings)
 
-        assert notes[0][0] == 'M'
-        assert notes[1][0] == 'K'
+        assert notes[0][0] == "M"
+        assert notes[1][0] == "K"
 
         formatted_notes = self._reformat_notes(notes)
-        notes_string = ' '.join(formatted_notes)
+        notes_string = " ".join(formatted_notes)
         self._convert_abc_to_midi(notes_string, filename)
