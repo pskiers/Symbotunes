214fd13c070eb574a0079072bab984a891bbf28e
Paweł Skierś <pawelsk269@gmail.com>
2024-04-21 23:47:50 +0200

    Update the folk-rnn model to match the original

--- models/folk_rnn/model.py
+++ models/folk_rnn/model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 from torch.optim import RMSprop
 from torch.optim.lr_scheduler import LambdaLR
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class FolkRNN(pl.LightningModule):
@@ -36,12 +37,11 @@ class FolkRNN(pl.LightningModule):
         if one_hot:
             # If one-hot encoding is used, initialize weights as an identity matrix
             self.weights_emb = nn.Parameter(torch.eye(vocab_size, dtype=torch.float32))
-            # weights_emb = torch.eye(vocab_size, dtype=torch.float32)
-            self.embedding = lambda x: torch.matmul(x.float(), self.weights_emb)
+            weights_emb = torch.eye(vocab_size, dtype=torch.float32)
         else:
             # Otherwise, initialize weights using Orthogonal initializer
             weights_emb = nn.init.orthogonal_(torch.empty(vocab_size, embedding_size))  # type: ignore
-            self.embedding = nn.Embedding.from_pretrained(weights_emb, freeze=True)
+        self.embedding = nn.Embedding.from_pretrained(weights_emb, freeze=True)
 
         rnn_input_size = vocab_size if one_hot else embedding_size
         self.lstm = nn.LSTM(
@@ -54,27 +54,35 @@ class FolkRNN(pl.LightningModule):
         )
         self.out = nn.Linear(in_features=lstm_size, out_features=vocab_size)
 
-    def forward(self, x: torch.Tensor, hidden: tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, lengths: torch.Tensor, hidden: tuple[torch.Tensor, torch.Tensor]
+    ) -> torch.Tensor:
         emb = self.embedding(x)
-        out, _ = self.lstm(emb, hidden)
+        emb_packed = pack_padded_sequence(emb, lengths, enforce_sorted=False, batch_first=True)
+        out, _ = self.lstm(emb_packed, hidden)
+        out, _ = pad_packed_sequence(out, batch_first=True, total_length=x.shape[1])
         out = self.out(out)
-        return out
+        return F.softmax(out, dim=2)
+
+    def _step(self, batch) -> torch.Tensor:
+        x, lengths = batch
+        y = x[:, 1:]
+        mask = (torch.arange(x.shape[1], device=self.device).unsqueeze(0) < lengths.unsqueeze(1)).float()
+        hidden = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
+        state = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
+        out = self(x, lengths.cpu(), (hidden, state))
+        corresponding_outs = out.gather(dim=2, index=y.unsqueeze(-1)).squeeze(-1)
+        log_probs = torch.log(corresponding_outs)
+        loss = -torch.mean(torch.sum(log_probs * mask[:, :-1], dim=1), dim=0)
+        return loss
 
     def training_step(self, batch, batch_idx):
-        x = batch
-        hidden = torch.zeros((self.num_layers, batch.shape[0], self.lstm_size), device=self.device)
-        state = torch.zeros((self.num_layers, batch.shape[0], self.lstm_size), device=self.device)
-        out = self(x, (hidden, state))
-        loss = F.cross_entropy(out[:, :-1], x[:, 1:])
+        loss = self._step(batch)
         self.log("train/loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x = batch
-        hidden = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
-        state = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
-        out = self(x, (hidden, state))
-        loss = F.cross_entropy(out[:, :-1], x[:, 1:])
+        loss = self._step(batch)
         self.log("val/loss", loss, prog_bar=True, logger=True, on_step=False, on_epoch=True)
         return loss
 
