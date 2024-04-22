7725dadcd3d66be707a724748a969d0c5b36cbfc
Paweł Skierś <pawelsk269@gmail.com>
2024-04-22 20:43:51 +0200

    Add sampling from folk rnn

--- models/folk_rnn/model.py
+++ models/folk_rnn/model.py
@@ -6,8 +6,10 @@ from torch.optim import RMSprop
 from torch.optim.lr_scheduler import LambdaLR
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
+from ..base import BaseModel
 
-class FolkRNN(pl.LightningModule):
+
+class FolkRNN(BaseModel):
     """LSTM-based music generation model, see https://arxiv.org/pdf/1604.08723.pdf"""
 
     def __init__(
@@ -20,7 +22,8 @@ class FolkRNN(pl.LightningModule):
         lr: float = 0.003,
         lr_decay: float = 0.97,
         lr_decay_start: int = 20,
-        max_sequence_length=100,
+        start_token: int = 135,
+        end_token: int = 136,
         *args: torch.Any,
         **kwargs: torch.Any
     ) -> None:
@@ -32,7 +35,10 @@ class FolkRNN(pl.LightningModule):
         self.lr = lr
         self.lr_decay = lr_decay
         self.lr_decay_start = lr_decay_start
-        self.max_sequence_length = max_sequence_length
+
+        self.start_token = start_token
+        self.end_token = end_token
+
         one_hot = embedding_size is None
         if one_hot:
             # If one-hot encoding is used, initialize weights as an identity matrix
@@ -54,12 +60,12 @@ class FolkRNN(pl.LightningModule):
         )
         self.out = nn.Linear(in_features=lstm_size, out_features=vocab_size)
 
-    def forward(
-        self, x: torch.Tensor, lengths: torch.Tensor, hidden: tuple[torch.Tensor, torch.Tensor]
-    ) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
         emb = self.embedding(x)
         emb_packed = pack_padded_sequence(emb, lengths, enforce_sorted=False, batch_first=True)
-        out, _ = self.lstm(emb_packed, hidden)
+        hidden = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
+        state = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
+        out, _ = self.lstm(emb_packed, (hidden, state))
         out, _ = pad_packed_sequence(out, batch_first=True, total_length=x.shape[1])
         out = self.out(out)
         return F.softmax(out, dim=2)
@@ -68,9 +74,7 @@ class FolkRNN(pl.LightningModule):
         x, lengths = batch
         y = x[:, 1:]
         mask = (torch.arange(x.shape[1], device=self.device).unsqueeze(0) < lengths.unsqueeze(1)).float()
-        hidden = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
-        state = torch.zeros((self.num_layers, x.shape[0], self.lstm_size), device=self.device)
-        out = self(x, lengths.cpu(), (hidden, state))
+        out = self(x, lengths.cpu())
         corresponding_outs = out.gather(dim=2, index=y.unsqueeze(-1)).squeeze(-1)
         log_probs = torch.log(corresponding_outs)
         loss = -torch.mean(torch.sum(log_probs * mask[:, :-1], dim=1), dim=0)
@@ -78,6 +82,8 @@ class FolkRNN(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         loss = self._step(batch)
+        lr = self.optimizers().param_groups[0]["lr"]
+        self.log("lr_abs", lr, prog_bar=True, logger=True, on_step=True, on_epoch=False)
         self.log("train/loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
         return loss
 
@@ -99,3 +105,19 @@ class FolkRNN(pl.LightningModule):
         )
         return {'optimizer': optimizer, 'lr_scheduler': scheduler}
     # fmt: on
+
+    @torch.no_grad()
+    def sample(self, batch_size: int) -> list[torch.Tensor]:
+        batch = torch.tensor([[self.start_token] for _ in range(batch_size)], device=self.device)
+        lengths = torch.tensor([1 for _ in range(batch_size)], device=torch.device("cpu"))
+        samples: list[torch.Tensor] = []
+        while batch.shape[0] > 0:
+            out = self(batch, lengths)
+            next_tokens = out[:, -1].argmax(dim=1).unsqueeze(1)
+            batch = torch.concat(tensors=(batch, next_tokens), dim=1)
+            ended = batch[:, -1] == self.end_token
+            samples += [sample for sample in batch[ended]]
+            batch = batch[~ended]
+            lengths += 1
+            lengths = lengths[~ended.cpu()]
+        return samples
